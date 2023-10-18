8c0f9f31e2736d31d70210dfe918f5fee6b35546
Danish Nayeem <idanish.ksa@gmail.com>
2023-10-18 16:14:59 +0300

    user details 6x6

--- src/app/components/user/user-shift/user-shift.component.ts
+++ src/app/components/user/user-shift/user-shift.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/common/api.service';
+import { ShiftService } from 'src/app/services/common/shift.service';
 import { UserService } from 'src/app/services/common/user.service';
-import { user_shift } from 'src/app/types/interfaces';
+import { shift, user_shift } from 'src/app/types/interfaces';
 
 @Component({
   selector: 'app-user-shift',
@@ -14,6 +15,7 @@ export class UserShiftComponent  implements OnInit {
   constructor(
     private api: ApiService,
     private usr: UserService,
+    private shf: ShiftService,
     private acr: ActivatedRoute
   ) { }
 
@@ -33,8 +35,23 @@ export class UserShiftComponent  implements OnInit {
     this.all = await this.usr.get_shifts(this.user_id);
   }
 
-  add_init(){
-    
+  async add_init(){
+    let s = await this.shf.selector();
+    if(s.role=='selected'){
+      if( this.all.filter(x=>x.shift_id==s.data.id).length>=1 )
+        this.api.Toast("Shift already alloted")
+      else{
+        await this.api.showLoader(this.ln.updating || "Updating")
+        let res = await this.usr.add_shift(this.user_id, s.data.id)
+        this.api.hideLoader()
+        if(res.ok){
+          this.all = res.data
+          this.api.Toast(this.ln.shift_alloted_to_user || "Shift alloted to the user")
+        }else{
+          this.api.Toast(this.ln.error_unable_to_assign_shift || "Error: Unable to assign shift")
+        }
+      }
+    }
   }
 
   remove(s: user_shift){
